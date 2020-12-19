5af771ef09152de57c414dd3a85f8c25db2aeb66
Giorgos Gial <g.gialouris@outlook.com.gr>
2020-12-20 00:40:40 +0200

    Add project files.

---
+++ ContosoAssignment/Models/Trainer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Assignment2.Models
+{
+    public class Trainer
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime HireDate { get; set; }
+    }
+}
